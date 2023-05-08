b994d30ff01fafd37de1ec6f8aa01c2b9c9f9ec5
rk28284 <112754760+rk28284@users.noreply.github.com>
2023-05-08 11:53:46 +0530

    completed

--- telepham/src/AllRoutes.jsx
+++ telepham/src/AllRoutes.jsx
@@ -6,17 +6,18 @@ import PageNotFound from './pages/PageNotFound';
 import Homepage from './pages/Homepage';
 
 import Main from "./components/Main";
+import AdminLogin from "./Page/AdminLogin";
 // import Admin from "./Page/AdminLogin"
 function AllRoutes(){
         return (
         <Routes>
-            {/* <Route path="/admin" element={<Admin />}></Route> */}
+           
             <Route path="/" element={<Homepage />} />
              <Route path="/medicine" element={<Main />} />
          
             <Route path='/login' element={<Login/>}/>
             <Route path='/sign-up' element={<SignUp/>}/>
-            {/* <Route path="/adminLogin" element={<AdminPage/>} /> */}
+            <Route path="/adminLogin" element={<AdminLogin />} />
             <Route path="*" element={<PageNotFound />} />
             
         </Routes>
